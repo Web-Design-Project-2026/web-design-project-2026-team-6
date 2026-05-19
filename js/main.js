30b3d9be4917d2f760c26a54ddbcef5a3cf9b4cc
alex <obal25ms@student.ju.se>
2026-05-19 22:11:35 +0200

    Add: working hamburger menu for mobile and favicon

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 
 //ELEMENT PATH
 const menuButton = document.getElementById("hamburger-menu-button");
+const menuIcon = document.querySelector(".hamburger-icon");
 const navLinks = document.getElementById("nav-links");
+
+//ASSET PATH
+const hamburgerOpen = "assets/hamburger-closed.svg";
+const hamburgerClosed = "assets/hamburger.svg";
 
 //VARIABLES
 let open = false;
@@ -12,9 +17,11 @@ menuButton.addEventListener("click", openCloseHamburgerMenu);
 function openCloseHamburgerMenu() {
   if (open === false) {
     navLinks.style.display = "flex";
+    menuIcon.src = hamburgerOpen;
     open = true;
   } else {
     navLinks.style.display = "none";
+    menuIcon.src = hamburgerClosed;
     open = false;
   }
 }
